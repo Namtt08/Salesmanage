edab22a03d81ce1cf49aafa28f19a0693512a1d7
Namtt <namttn08@gmail.com>
2022-06-30 00:52:54 +0700

    commit code

---
+++ ProjectManage/src/main/java/org/project/manage/response/makettingResponse.java
@@ -0,0 +1,18 @@
+package org.project.manage.response;
+
+import lombok.Data;
+
+@Data
+public class makettingResponse {
+	
+	private String codeOrders;
+	
+	private String chargeType;
+	
+	private Long amount;
+	
+	private String description;
+	
+	private String createdDate;
+	
+}
